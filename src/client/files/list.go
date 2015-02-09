97b3abd5402c642069b76f7e7d4e833f5047191e
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-02-10 00:43:53 +0100

    Implemented JSON File sending informations

--- src/client/files/list.go
+++ src/client/files/list.go
@@ -9,33 +9,59 @@ package files
 
 import (
 	"bitbucket.org/ebianchi/memento-common/common"
-	"fmt"
 	"net"
 	"os"
 	"path/filepath"
+	"runtime"
+	"syscall"
 )
 
-func visitfile(fp string, fi os.FileInfo, err error) error {
-	if err != nil {
-		fmt.Println(err) // can't walk here,
-		return nil       // but continue walking elsewhere
-	}
+var connection net.Conn
 
-	// not a file
-	if fi.IsDir() {
-		fmt.Println(fp)
+func visitfile(fp string, fi os.FileInfo, err error) error {
+	file := common.JSONFile{}
+
+	if err != nil {
+		res := common.JSONResult{"ko", err.Error()}
+		res.Send(connection)
 		return nil
 	}
 
-	fmt.Println(fp)
+	// Set the file name and the operating system
+	file.Name = fp
+	file.Os = runtime.GOOS
+
+	if runtime.GOOS == "linux" {
+		// FIXME: Convert UID into Username (you need to use a C call)
+		//file.User = fi.Sys().(*syscall.Stat_t).Uid
+
+		// FIXME: Convert GID into Groupname (you need to use a C call)
+		//file.Group = fi.Sys().(*syscall.Stat_t).Gid
+	}
+
+	// Set type of element (file or directory)
+	if fi.IsDir() {
+		file.Type = "directory"
+	} else {
+		file.Type = "file"
+	}
+
+	// TODO: add hash, size, date, permission and ACL
+
+	// Set result
+	file.Result = "ok"
+	file.Send(connection)
 	return nil
 }
 
 func List(conn net.Conn, command *common.JSONCommand) {
+	connection = conn
+
 	if command.Directory != "" {
+		// WARNING: filepath.Walk() is inefficient
 		filepath.Walk(command.Directory, visitfile)
 	} else {
 		res := common.JSONResult{"ko", "No directory specified"}
-		res.Send(conn)
+		res.Send(connection)
 	}
 }
