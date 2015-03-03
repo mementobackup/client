96d090e4643b8af912e05e67cc5feb5fad7a61a1
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-03-03 17:21:06 +0100

    Managed return values

--- src/client/files/list.go
+++ src/client/files/list.go
@@ -34,8 +34,8 @@ func visitfile(fp string, fi os.FileInfo, err error) error {
 	file.Mtime = fi.ModTime().Unix()
 
 	if runtime.GOOS == "linux" {
-		file.User = posix_user(fi)
-		file.Group = posix_group(fi)
+		file.User, _ = posix_user(fi)
+		file.Group, _ = posix_group(fi)
 		file.Mode = fi.Mode().String()
 	}
 
