c108a41115e74fafcbc170eae9548d7b6ef6c4d1
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-04-02 11:00:04 +0200

    Changed rules of building package

--- src/client/files/posix.go
+++ src/client/files/posix.go
@@ -1,11 +1,11 @@
+// +build !windows
+
 /*
  Copyright (C) 2015 Enrico Bianchi (enrico.bianchi@gmail.com)
  Project       Memento
  Description   A backup system
  License       GPL version 2 (see GPL.txt for details)
 */
-
-// +build !windows
 
 package files
 
